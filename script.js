b55974c42d333998be6aed7f6ecc8051bbb40df8
Strahinja <1711365+consistencyIncarnate@users.noreply.github.com>
2025-06-10 13:30:22 +0200

    Add logic for computer choice

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Get computer choice randomly 
+function getComputerChoice() {
+    // Storage for possible choices
+    const choice = ["rock", "paper", "scissors"]
+    // Storage for random value in 0-2 range
+    let randomVal = Math.floor(Math.random() * 3);
+    return choice[randomVal];
+}
+
 // Get user input
 // Play one round 
 // Play whole game (BO3) 
