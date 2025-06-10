efe2ef42e670071a2062d3b9222fdef971fe5fc2
Strahinja <1711365+consistencyIncarnate@users.noreply.github.com>
2025-06-10 18:45:46 +0200

    Add logic to play multiple rounds thus finishing the game

--- script.js
+++ script.js
@@ -11,41 +11,56 @@ function getHumanChoice() {
     let input = prompt("Please enter your choice:");
     return input;
 }
-// Global storage for choices
-const humanSelection = getHumanChoice().toLowerCase();
-const computerSelection = getComputerChoice();
-// Global storage for player scores
-let humanScore = 0;
-let computerScore = 0;
-// Play one round 
-function playRound(humanChoice, computerChoice) {
-    // Rock > Scissors 
-    if(humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You win! Rock beats Scissors.")
-        humanScore++;
-      // Paper > Rock
-    } else if(humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You win! Paper beats Rock.")
-        humanScore++;
-      // Scissors > Paper
-    } else if(humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You win! Scissors beat Paper.")
-        humanScore++;
-      // Rock < Paper
-    } else if(humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beats Rock.")
-        computerScore++;
-      // Paper < Scissors
-    } else if(humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose! Paper beats Rock.")
-        computerScore++;
-      // Scissors < Rock
-    } else if(humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! Rock beats Scissors.")
-        computerScore++;
+// Play whole game (BO5)
+function playGame() {
+    // Storage for player scores
+    let humanScore = 0;
+    let computerScore = 0;
+    // Play one round 
+    function playRound(humanChoice, computerChoice) {
+        // Rock > Scissors 
+        if(humanChoice === "rock" && computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors.")
+            humanScore++;
+          // Paper > Rock
+        } else if(humanChoice === "paper" && computerChoice === "rock") {
+            console.log("You win! Paper beats Rock.")
+            humanScore++;
+          // Scissors > Paper
+        } else if(humanChoice === "scissors" && computerChoice === "paper") {
+            console.log("You win! Scissors beat Paper.")
+            humanScore++;
+          // Rock < Paper
+        } else if(humanChoice === "rock" && computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock.")
+            computerScore++;
+          // Paper < Scissors
+        } else if(humanChoice === "paper" && computerChoice === "scissors") {
+            console.log("You lose! Paper beats Rock.")
+            computerScore++;
+          // Scissors < Rock
+        } else if(humanChoice === "scissors" && computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors.")
+            computerScore++;
+        } else {
+            console.log("Draw!")
+        }
+    }
+    // Loop to play multiple rounds
+    for (let i = 0; i < 5; i++) {
+        // Storage for choices
+        let humanSelection = getHumanChoice().toLowerCase();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection); 
+    }
+    // Final declaration
+    if (humanScore > computerScore) {
+        console.log(`You won! The score is ${humanScore} : ${computerScore} in your favor.`)
+    } else if (humanScore === computerScore) {
+        console.log(`It's a draw! The score is ${humanScore} : ${computerScore}.`)
     } else {
-        console.log("Draw!")
+        console.log(`You lost! The score is ${humanScore} : ${computerScore} in the computers favor.`)
     }
 }
-playRound(humanSelection, computerSelection);
-// Play whole game (BO3) 
+playGame();
